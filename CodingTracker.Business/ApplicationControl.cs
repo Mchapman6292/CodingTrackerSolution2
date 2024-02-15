96553cd85292f941b85bf6ab0faa0c7712dba990
Mchapman <mchapman6292@outlook.com>
2024-02-15 17:41:20 +1000

    Added login screen controls, ApplcationController class & interface

---
+++ CodingTracker.Business/ApplicationControl.cs
@@ -0,0 +1,30 @@
+﻿using CodingTracker.Common.IApplicationControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.Business.ApplicationControls
+{
+    public class ApplicationControl : IApplicationControl
+    {
+        public bool ApplicationIsRunning { get; private set; }
+
+
+        public ApplicationController()
+        {
+            ApplicationIsRunning = false; // Set to false instead of true to ensure that processes don't run or exit prematurely or unintentionally.
+        }
+
+        public void StartApplication()
+        {
+            ApplicationIsRunning = true;
+        }
+
+        public void ExitApplication() 
+        {
+            ApplicationIsRunning = false; 
+        }
+    }
+}
