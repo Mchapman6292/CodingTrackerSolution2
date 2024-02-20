7f747d1ac0a674311cb5a11823c8a618fff9ba66
Mchapman <mchapman6292@outlook.com>
2024-02-20 18:09:39 +1000

    Added credentialservice class, & interface, changes to credential management

---
+++ CodingTracker.Data/LoginManager.cs
@@ -0,0 +1,41 @@
+﻿using CodingTracker.Common.ILoginManagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.Data.LoginManagers
+{
+    public class LoginManager : ILoginManager
+    {
+
+        public LoginManager() 
+        {
+
+
+        }
+
+        public bool ValidateLoginCredentials(string username, string password)
+        {
+            string hashedPassword = HashPassword(password);
+            return _credentialsDict.Values.Any(credential =>
+                credential.Username == username && credential.Password == hashedPassword);
+        }
+
+        public bool CheckUserName(string username)
+        {
+            return _credentialsDict.Values.Any(credential => credential.Username == username);
+        }
+
+        public bool CheckUserId(int userId)
+        {
+            return _credentialsDict.ContainsKey(userId);
+        }
+
+        public bool CheckUserPassword(string password)
+        {
+            return _credentialsDict.Values.Any(credential => credential.Password == password);
+        }
+    }
+}
---
+++ CodingTracker.Data/UserCredentialDTOs.cs
@@ -0,0 +1,19 @@
+﻿using CodingTracker.Common.IUserCredentialDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.Data.UserCredentialDTOs
+{
+    public class UserCredentialDTO : IUserCredentialDTO
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+
+    }
+}
