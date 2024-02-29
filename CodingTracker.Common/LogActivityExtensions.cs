e9ec02b554c81ec1f5bea19afdc0b43e56176568
Mchapman <mchapman6292@outlook.com>
2024-02-29 12:47:48 +1000

    Logging changes.

---
+++ CodingTracker.Common/LogActivityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.Common
+{
+    internal static class ActivityExtensions
+    {
+        public static string GetSpanId(this Activity activity) => activity?.SpanId.ToHexString() ?? string.Empty;
+        public static string GetTraceId(this Activity activity) => activity?.TraceId.ToHexString() ?? string.Empty;
+        public static string GetParentId(this Activity activity) => activity?.ParentSpanId.ToHexString() ?? string.Empty;
+    }
+}
